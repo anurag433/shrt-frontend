085310109f414df6e8f9ac5614d9c53dff8fa2c7
Anurag Singh <anurag504singh@gmail.com>
2026-03-29 18:42:42 +0530

    feat: improve UX with validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import React , {useState} from 'react'
 import Navbar from './component/Navbar'
 import Card from './component/Card'
 import axios from './api/axios'
-
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 function App() {
 
   const [url, setUrl] = useState("");
   const [shortUrl, setShortUrl] = useState("");
 
+  const isValidUrl = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+  
   const handleSubmit = async () => {
+    if (!isValidUrl(url)) {
+      toast.error("Invalid URL");
+      return;
+    }
     try {
       const response = await axios.post(
         "http://127.0.0.1:8000/shorten/",
@@ -22,12 +36,13 @@ function App() {
 
     } catch (error) {
       console.error("Error:", error);
+      toast.error("Something went wrong!");
     }
   };
 
-
   return (
     <div className='min-h-screen bg-gradient-to-br from-[#D5EFFB] via-[#E6E9FA] to-[#BBB7C3]'>
+      <Toaster position="top-center" />
       <Navbar/>
       <div className='flex justify-center items-center h-[60vh]'>
         <Card
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,8 +1,13 @@
 import React,{ useState } from 'react'
 import {ChevronRight, Copy} from 'lucide-react'
+import toast from "react-hot-toast";
 
 function Card(props) {
 
+    const handleCopy = () => {
+        navigator.clipboard.writeText(props.shortUrl);
+        toast.success("Copied!");
+    }
     return (
     <div className="bg-white/20 backdrop-blur-lg border border-white/30 rounded-2xl p-10 shadow-xl w-full max-w-xl">
       <h1 className="text-3xl text-center mb-8 text-gray-800 font-medium">
@@ -14,7 +19,8 @@ function Card(props) {
         <input
           type="text"
           value={props.url}
-          onChange={(e) => {props.setUrl(e.target.value)}}
+          onChange={(e) => {props.setUrl(e.target.value) ;
+          }}
           
           placeholder="https://www.example-long-url.com/..."
           className="flex-1 px-4 py-3 outline-none text-gray-700 bg-transparent"
@@ -23,25 +29,28 @@ function Card(props) {
         <button 
         onClick={props.handleSubmit}
         className="bg-gradient-to-r from-[#1F2B44] to-[#0f172a] text-white px-6 py-2 flex gap-1 rounded-lg active:scale-95 font-semibold text-base">
-          Shorten <ChevronRight className='text-bold'/>
+          Shorten <ChevronRight />
         </button>
       </div>
 
-     {/* <div className="flex items-center bg-white border border-gray-200 rounded-xl p-2 shadow-sm">
-
-        <input
-          type="text"
-          value={shortUrl}
-          className="flex-1 px-4 py-3 outline-none text-gray-700 bg-transparent"
-        />
-
-        <button className="bg-gradient-to-r from-[#1F2B44] to-[#0f172a] text-white px-3 py-2 flex gap-3 rounded-lg active:scale-95 font-semibold text-base ">
-          <Copy size={20} strokeWidth={2} className='text-3xl' />
-        </button>
-      </div> */}
+     {props.shortUrl && (
+        <div className="flex items-center bg-white border border-gray-200 rounded-xl p-2 shadow-sm">
+            <a
+            href={props.shortUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-1 px-4 py-3 text-blue-600 underline break-all"
+            >
+                {props.shortUrl}
+            </a>
+            <button 
+            onClick={handleCopy}
+            className="bg-gradient-to-r from-[#1F2B44] to-[#0f172a] text-white px-3 py-2 flex gap-3 rounded-lg active:scale-95 font-semibold text-base hover:scale-105 ">
+            <Copy size={20} strokeWidth={2} className='text-3xl' />
+            </button>
+        </div>
+    )}
       </div>
-      
-
      </div>
     )
 }
