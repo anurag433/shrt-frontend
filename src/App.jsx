90aff84e677e5f595dfcc4e115389e6db2726719
Anurag Singh <anurag504singh@gmail.com>
2026-03-29 12:06:13 +0530

    feat: integrate axios for API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Navbar from './component/Navbar'
 import Card from './component/Card'
+import axios from './api/axios'
+
 
 function App() {
+
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/shorten/",
+        {
+          original_url: url,
+        }
+      );
+
+      setShortUrl(response.data.short_url);
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+
   return (
     <div className='min-h-screen bg-gradient-to-br from-[#D5EFFB] via-[#E6E9FA] to-[#BBB7C3]'>
       <Navbar/>
       <div className='flex justify-center items-center h-[60vh]'>
-        <Card/>
+        <Card
+          url={url}
+          setUrl={setUrl}
+          handleSubmit={handleSubmit}
+          shortUrl={shortUrl}
+        />
       </div>
     </div>
   )
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
-import {ChevronRight} from 'lucide-react'
-function Card() {
+import React,{ useState } from 'react'
+import {ChevronRight, Copy} from 'lucide-react'
+
+function Card(props) {
+
     return (
     <div className="bg-white/20 backdrop-blur-lg border border-white/30 rounded-2xl p-10 shadow-xl w-full max-w-xl">
       <h1 className="text-3xl text-center mb-8 text-gray-800 font-medium">
         Transform Your Long URLs
       </h1>
-      <div className="flex items-center bg-white border border-gray-200 rounded-xl p-2 shadow-sm">
+      <div className='flex flex-col gap-10'>
+        <div className="flex items-center bg-white border border-gray-200 rounded-xl p-2 shadow-sm">
 
         <input
           type="text"
+          value={props.url}
+          onChange={(e) => {props.setUrl(e.target.value)}}
+          
           placeholder="https://www.example-long-url.com/..."
           className="flex-1 px-4 py-3 outline-none text-gray-700 bg-transparent"
         />
 
-        <button className="bg-gradient-to-r from-[#1F2B44] to-[#0f172a] text-white px-6 py-2 flex gap-1 rounded-lg active:scale-95 font-semibold text-base">
+        <button 
+        onClick={props.handleSubmit}
+        className="bg-gradient-to-r from-[#1F2B44] to-[#0f172a] text-white px-6 py-2 flex gap-1 rounded-lg active:scale-95 font-semibold text-base">
           Shorten <ChevronRight className='text-bold'/>
         </button>
       </div>
-    </div>
+
+     {/* <div className="flex items-center bg-white border border-gray-200 rounded-xl p-2 shadow-sm">
+
+        <input
+          type="text"
+          value={shortUrl}
+          className="flex-1 px-4 py-3 outline-none text-gray-700 bg-transparent"
+        />
+
+        <button className="bg-gradient-to-r from-[#1F2B44] to-[#0f172a] text-white px-3 py-2 flex gap-3 rounded-lg active:scale-95 font-semibold text-base ">
+          <Copy size={20} strokeWidth={2} className='text-3xl' />
+        </button>
+      </div> */}
+      </div>
+      
+
+     </div>
     )
 }
 
